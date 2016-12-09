c5e3b291725d63d9bc7d14f7461cb7556b3fc7c6
codeamac18 <codeamac18@gmail.com>
2016-12-09 17:56:30 -0600

    finished app

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,8 +5,24 @@ get '/' do
   erb :index
 end
 
-post '/' do
+post '/log_in' do
+  @email = params[:email]
+  @password = params[:pass]
+  
+  if User.authenticate(@email, @password)
+    
+    @user = User.find_by(email: @email)
+    session[:id] = @user.id
+    redirect to "/users_home/#{@user.id}"
 
+  else
+    redirect to '/log_in'
+  end
+
+end
+
+get '/log_in' do
+  erb :log_in
 end
 
 get '/new_user' do
@@ -17,11 +33,11 @@ end
 
 post '/new_user' do
 
-  @user = User.new(name: params[:fullname], email: params[:email], password: params[:pas] )
+  @user = User.new(name: params[:fullname], email: params[:email], password: params[:pass] )
 
   if @user.save
     session[:id] = @user.id
-    erb :profile
+    redirect to "/users_home/#{@user.id}"
   else
     "ERROR"
     erb :sign_in 
@@ -29,7 +45,22 @@ post '/new_user' do
 
 
 end
+ 
+ before '/users_home/:id'  do
+    if session[:id] == nil
+      redirect to '/log_in'
+    end
+  end
+
+get '/users_home/:id' do
+ @user = User.find(session[:id])
+ erb :profile
+end
 
 
-name ||= "MAU"
-name = name || "MAU"
+
+
+get '/log_out' do 
+  session.clear
+  redirect to '/'  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,20 @@
 class User < ActiveRecord::Base
-  
+
   validates :name, :email, presence: true
   validates :email, uniqueness: true
   validates :password, length: {in: 6..15}
   # Remember to create a migration!
 
-  # ej. User.authenticate('fernando@codea.mx', 'qwerty')
-  def self.new_user(*args)
 
-  end
+ 
+
+  # ej. User.authenticate('fernando@codea.mx', 'qwerty')
   def self.authenticate(email, password)
 
-    # si el email y el password corresponden a un usuario valido, regresa el usuario
-    # de otra manera regresa nil
+    User.find_by(email: email, password: password)   
   end
+
+
+
 
 end
